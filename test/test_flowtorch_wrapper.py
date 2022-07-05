131fa5bbaae245125e92169db1bcd88313046014
Alexander Johan Arntzen <alexander.arntzen@gmail.com>
2022-07-05 21:00:44 +0200

    clean up code

--- test/test_flowtorch_wrapper.py
+++ test/test_flowtorch_wrapper.py
@@ -1,13 +1,16 @@
+"""Test that flowtorch wrapper is implemented correctly"""
 import unittest
 
 import torch
-import torch.distributions.constraints as constraints
 import flowtorch.distributions as ftdist
 import shapeflow as sf
 
 
 class TestFlowTorchWrapper(unittest.TestCase):
+    """"""
+
     def test_normflow(self):
+        """Test flowtorch with resdiual transform from normflow"""
         # Set up model
         num_layers = 2
         dims = 2
@@ -30,6 +33,7 @@ class TestFlowTorchWrapper(unittest.TestCase):
         print(samples.shape)
 
     def test_normflow_2(self):
+        """Test flowtorch with resdiual transform from normflow"""
         # Set up model
         num_layers = 2
         dims = 2
@@ -49,12 +53,6 @@ class TestFlowTorchWrapper(unittest.TestCase):
         )
         samples = flow.sample([10])
         print(samples.shape)
-
-    def test_shape(self):
-        base_dist = torch.distributions.Normal(0, 1)
-        print(base_dist.event_shape)
-        domain = constraints.independent(constraints.real, len(base_dist.event_shape))
-        print(domain.event_dim)
 
 
 if __name__ == "__main__":
--- test/test_utils.py
+++ test/test_utils.py
@@ -6,8 +6,11 @@ import torch
 
 class TestUtils(unittest.TestCase):
     def test_conversion(self):
+        """Test conversion between data and animations is correct"""
         size = (10, 20)
         data = torch.rand(size) * 2 * torch.pi - torch.pi
+
+        # test inverse of forward is correct
         diff = utils.motion_array_to_data(utils.data_to_motion_array(data)) - data
 
         max_diff = torch.max(torch.abs(diff)).item()
